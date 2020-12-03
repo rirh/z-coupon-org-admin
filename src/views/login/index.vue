9407f4a67bfd3174eb28c7b2b90e447f1974e796
charlesbosso <73486773+charlesbosso@users.noreply.github.com>
2020-12-04 00:23:46 +0800

    delete default login info

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -9,7 +9,7 @@
       label-position="left"
     >
       <div class="title-container">
-        <h3 class="title">{{ name }}管理系统</h3>
+        <h3 class="title">{{ name }}</h3>
       </div>
 
       <el-form-item prop="username">
@@ -83,8 +83,8 @@ export default {
     return {
       name: packageConfig.name,
       loginForm: {
-        username: 'admin',
-        password: '123456'
+        username: '',
+        password: ''
       },
       loginRules: {
         username: [
